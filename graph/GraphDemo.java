96cbc7f32a3416db67f31b0a9e63e7aaaa18e1b1
sriharigaddam966-web <sriharigaddam966@gmail.com>
2025-12-21 18:59:32 +0530

    Create graph/GraphDemo.java
    Added graph implementation using BFS

---
+++ graph/GraphDemo.java
@@ -0,0 +1,51 @@
+import java.util.*;
+
+public class GraphDemo {
+    private int vertices;
+    private LinkedList<Integer>[] adjList;
+
+    GraphDemo(int v) {
+        vertices = v;
+        adjList = new LinkedList[v];
+        for (int i = 0; i < v; i++) {
+            adjList[i] = new LinkedList<>();
+        }
+    }
+
+    void addEdge(int src, int dest) {
+        adjList[src].add(dest);
+        adjList[dest].add(src); // Undirected graph
+    }
+
+    void BFS(int start) {
+        boolean[] visited = new boolean[vertices];
+        Queue<Integer> queue = new LinkedList<>();
+
+        visited[start] = true;
+        queue.add(start);
+
+        while (!queue.isEmpty()) {
+            int node = queue.poll();
+            System.out.print(node + " ");
+
+            for (int neighbor : adjList[node]) {
+                if (!visited[neighbor]) {
+                    visited[neighbor] = true;
+                    queue.add(neighbor);
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        GraphDemo g = new GraphDemo(5);
+
+        g.addEdge(0, 1);
+        g.addEdge(0, 2);
+        g.addEdge(1, 3);
+        g.addEdge(1, 4);
+
+        System.out.print("BFS Traversal: ");
+        g.BFS(0);
+    }
+}
